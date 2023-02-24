d1b1c7c1dd6f036eedec8fdd6dcde86cb8edea11
Conor Coker <conorcoker@gmail.com>
2023-02-24 15:52:56 +0000

    Added a menu with some basic input functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,63 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    runMenu()
+
+}
+
+private fun runMenu() {
+    do {
+        when (displayMenuAndReturnInput()) {
+            1 -> addNote()
+            2 -> listNotes()
+            3 -> updateNote()
+            4 -> deleteNote()
+            0 -> exitApp()
+            else -> println("Invalid option entered")
+        }
+    } while (true)
+}
+
+private fun exitApp() {
+    TODO("Not yet implemented")
+}
+
+private fun deleteNote() {
+    TODO("Not yet implemented")
+}
+
+private fun updateNote() {
+    TODO("Not yet implemented")
+}
+
+private fun listNotes() {
+    TODO("Not yet implemented")
+}
+
+private fun addNote() {
+    TODO("Not yet implemented")
+}
+
+private fun displayMenuAndReturnInput(): Int {
+
+    val scanner = Scanner(System.`in`)
+
+    println(
+        """
+        --------------------
+        NOTE KEEPER APP
+        --------------------
+        NOTE MENU
+            1) Add a note
+            2) List all notes
+            3) Update a note
+            4) Delete a note
+        --------------------
+            0) Exit
+        --------------------    
+    """.trimIndent()
+    )
+    print("==>> ")
+    return scanner.nextInt()
 }
